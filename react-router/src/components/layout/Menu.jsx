117f373714bf8c6397a4ecef2b1bb3ac8497a586
bryan <bryan.oliveira12345@gmail.com>
2024-04-26 10:33:35 -0300

    Styles

--- react-router/src/components/layout/Menu.jsx
+++ react-router/src/components/layout/Menu.jsx
@@ -2,5 +2,18 @@ import React from 'react';
 import './Menu.css';
 
 export default function Menu(props) {
-  return <aside className="Menu">Menu</aside>;
+  return (
+    <aside className="Menu">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
--- react-router/src/views/App.jsx
+++ react-router/src/views/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+import { BrowserRouter } from 'react-router-dom';
 
 export default function App(props) {
-  return <div className="App">Component App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Menu />
+        <Content />
+      </BrowserRouter>
+    </div>
+  );
 }
