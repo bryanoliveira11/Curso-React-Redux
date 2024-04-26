94001f45e59776c31c707248737f5e52e5606168
bryan <bryan.oliveira12345@gmail.com>
2024-04-26 10:12:14 -0300

    Initial

---
+++ react-router/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>App</h1>
+    </div>
+  </React.StrictMode>,
+);
