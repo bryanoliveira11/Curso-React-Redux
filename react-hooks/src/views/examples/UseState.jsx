4fdf5724e5ed400eeaa0c51cf9273e42f9fde814
bryan <bryan.oliveira12345@gmail.com>
2024-05-06 10:45:51 -0300

    useState #2

--- react-hooks/src/views/examples/UseState.jsx
+++ react-hooks/src/views/examples/UseState.jsx
@@ -4,6 +4,7 @@ import SectionTitle from '../../components/layout/SectionTitle';
 
 const UseState = (props) => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
 
   return (
     <div className="useState">
@@ -30,6 +31,13 @@ const UseState = (props) => {
         </div>
       </div>
       <SectionTitle title="Exercise #02" />
+      <input
+        type="text"
+        className="input"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <span className="text">{name}</span>
     </div>
   );
 };
