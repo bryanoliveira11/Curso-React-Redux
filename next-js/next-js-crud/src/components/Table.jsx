2fb8101046d4dc5f5624e136e8b06345659d6b54
bryan <bryan.oliveira12345@gmail.com>
2024-05-15 10:15:11 -0300

    Table #2

--- next-js/next-js-crud/src/components/Table.jsx
+++ next-js/next-js-crud/src/components/Table.jsx
@@ -2,9 +2,9 @@ export default function Table(props) {
   function renderTableHead() {
     return (
       <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Age</th>
+        <th className="text-left p-4">ID</th>
+        <th className="text-left p-4">Name</th>
+        <th className="text-left p-4">Age</th>
       </tr>
     );
   }
@@ -12,18 +12,22 @@ export default function Table(props) {
   function renderData() {
     return props.clients?.map((client, index) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr key={client.id} className={`${index % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}>
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{renderTableHead()}</thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`bg-gradient-to-r from-purple-500 to-purple-800 text-gray-50`}
+      >
+        {renderTableHead()}
+      </thead>
       <tbody>{renderData()}</tbody>
     </table>
   );
