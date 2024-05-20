df1280a60259b23c4fc944cab8d4cf57aafa78d9
bryan <bryan.oliveira12345@gmail.com>
2024-05-20 10:44:50 -0300

    Api config

--- react-todo-app/backend/src/config/database.js
+++ react-todo-app/backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect("mongodb://localhost/todo", {
+  .connect("mongodb://localhost:27017/todo", {
     useMongoClient: true,
   })
   .then(() => {
