ed0c7e195b3b71abf3f6cf186f080d2036b65fce
bryan <bryan.oliveira12345@gmail.com>
2024-05-21 14:02:34 -0300

    Mongoose Error Adjust

--- react-todo-app/backend/src/config/database.js
+++ react-todo-app/backend/src/config/database.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose
-  .connect("mongodb://localhost:27017/todo", {
-    useMongoClient: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+mongoose.connect("mongodb://localhost:27017/todo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
